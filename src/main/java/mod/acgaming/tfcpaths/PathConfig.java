e37e71604a2179cadb0d60093f40ac10dd5a4689
ACGaming <4818419+ACGaming@users.noreply.github.com>
2021-07-03 14:27:55 +0200

    Split player and mob settings

--- src/main/java/mod/acgaming/tfcpaths/PathConfig.java
+++ src/main/java/mod/acgaming/tfcpaths/PathConfig.java
@@ -6,27 +6,59 @@ import net.minecraftforge.fml.client.event.ConfigChangedEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@Config(modid = TFCPaths.MODID, name = "TFCPaths", category = "general")
+@Config(modid = TFCPaths.MODID, name = "TFCPaths")
 public class PathConfig
 {
-    @Config.Name("Grass -> Dirt")
-    @Config.Comment("Chance per mille for grass to turn into dirt, set to 0.0 to disable")
-    @Config.RangeDouble(min = 0.0, max = 1000.0)
-    public static double GRASS_TO_DIRT = 25.0;
+    public static GeneralSettings general_settings = new GeneralSettings();
+    public static PlayerSettings player_settings = new PlayerSettings();
+    public static MobSettings mob_settings = new MobSettings();
 
-    @Config.Name("Dirt -> Path")
-    @Config.Comment("Chance per mille for dirt to turn into a path, set to 0.0 to disable")
-    @Config.RangeDouble(min = 0.0, max = 1000.0)
-    public static double DIRT_TO_PATH = 10.0;
+    public static class GeneralSettings
+    {
+        @Config.Name("Debug Mode")
+        @Config.Comment("When enabled, prints debug values to console")
+        public static boolean DEBUG = false;
 
-    @Config.Name("Path -> Gravel")
-    @Config.Comment("Chance per mille for a path to turn into gravel, set to 0.0 to disable")
-    @Config.RangeDouble(min = 0.0, max = 1000.0)
-    public static double PATH_TO_GRAVEL = 1.0;
+        @Config.Name("All Entities")
+        @Config.Comment("When enabled, all entities create paths (performance intensive)")
+        public static boolean ALL_ENTITIES = false;
+    }
 
-    @Config.Name("Debug Mode")
-    @Config.Comment("Switch for debug mode (print chances to console)")
-    public static boolean DEBUG = false;
+    public static class PlayerSettings
+    {
+        @Config.Name("Grass -> Dirt")
+        @Config.Comment("Chance per mille for grass to turn into dirt, set to 0.0 to disable")
+        @Config.RangeDouble(min = 0.0, max = 1000.0)
+        public static double GRASS_TO_DIRT = 30.0;
+
+        @Config.Name("Dirt -> Path")
+        @Config.Comment("Chance per mille for dirt to turn into a path, set to 0.0 to disable")
+        @Config.RangeDouble(min = 0.0, max = 1000.0)
+        public static double DIRT_TO_PATH = 20.0;
+
+        @Config.Name("Path -> Gravel")
+        @Config.Comment("Chance per mille for a path to turn into gravel, set to 0.0 to disable")
+        @Config.RangeDouble(min = 0.0, max = 1000.0)
+        public static double PATH_TO_GRAVEL = 2.0;
+    }
+
+    public static class MobSettings
+    {
+        @Config.Name("Grass -> Dirt")
+        @Config.Comment("Chance per mille for grass to turn into dirt, set to 0.0 to disable")
+        @Config.RangeDouble(min = 0.0, max = 1000.0)
+        public static double GRASS_TO_DIRT = 15.0;
+
+        @Config.Name("Dirt -> Path")
+        @Config.Comment("Chance per mille for dirt to turn into a path, set to 0.0 to disable")
+        @Config.RangeDouble(min = 0.0, max = 1000.0)
+        public static double DIRT_TO_PATH = 10.0;
+
+        @Config.Name("Path -> Gravel")
+        @Config.Comment("Chance per mille for a path to turn into gravel, set to 0.0 to disable")
+        @Config.RangeDouble(min = 0.0, max = 1000.0)
+        public static double PATH_TO_GRAVEL = 1.0;
+    }
 
     @Mod.EventBusSubscriber(modid = TFCPaths.MODID)
     private static class EventHandler
--- src/main/java/mod/acgaming/tfcpaths/PathEvent.java
+++ src/main/java/mod/acgaming/tfcpaths/PathEvent.java
@@ -3,6 +3,7 @@ package mod.acgaming.tfcpaths;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
@@ -21,7 +22,7 @@ public class PathEvent
     @SubscribeEvent(priority = EventPriority.HIGHEST)
     public static void createPath(LivingEvent.LivingUpdateEvent event)
     {
-        if (event.getEntity() != null)
+        if (event.getEntity() != null && event.getEntity() instanceof EntityPlayer)
         {
             EntityLivingBase entity = event.getEntityLiving();
             int posX = MathHelper.floor(entity.posX);
@@ -34,16 +35,13 @@ public class PathEvent
             double random = Math.random() * 1000.0D + 1.0D;
             double speed = Math.sqrt(entity.motionX * entity.motionX + entity.motionY * entity.motionY + entity.motionZ * entity.motionZ);
 
-            if (PathConfig.DEBUG)
-                System.out.println(random);
-
             if (!world.isRemote && state.getBlock() instanceof BlockRockVariant && speed > 0.08)
             {
                 BlockRockVariant blockRock = (BlockRockVariant) state.getBlock();
 
                 if (blockRock.getType() == Rock.Type.GRASS || blockRock.getType() == Rock.Type.DRY_GRASS)
                 {
-                    if (random < PathConfig.GRASS_TO_DIRT)
+                    if (random < PathConfig.PlayerSettings.GRASS_TO_DIRT)
                     {
                         world.setBlockState(posEntity, BlockRockVariant.get(blockRock.getRock(), Rock.Type.DIRT).getDefaultState());
                         BlockPos upPos = posEntity.up();
@@ -58,7 +56,7 @@ public class PathEvent
 
                 if (blockRock.getType() == Rock.Type.DIRT)
                 {
-                    if (random < PathConfig.DIRT_TO_PATH)
+                    if (random < PathConfig.PlayerSettings.DIRT_TO_PATH)
                     {
                         world.setBlockState(posEntity, BlockRockVariant.get(blockRock.getRock(), Rock.Type.PATH).getDefaultState());
                         return;
@@ -67,10 +65,71 @@ public class PathEvent
 
                 if (blockRock.getType() == Rock.Type.PATH)
                 {
-                    if (random < PathConfig.PATH_TO_GRAVEL)
+                    if (random < PathConfig.PlayerSettings.PATH_TO_GRAVEL)
                     {
                         world.setBlockState(posEntity, BlockRockVariant.get(blockRock.getRock(), Rock.Type.GRAVEL).getDefaultState());
                         return;
+                    }
+                }
+            }
+        }
+
+        if (PathConfig.GeneralSettings.ALL_ENTITIES)
+        {
+            if (event.getEntity() != null & !(event.getEntity() instanceof EntityPlayer))
+            {
+                EntityLivingBase entity = event.getEntityLiving();
+                int posX = MathHelper.floor(entity.posX);
+                int posY = MathHelper.floor(entity.getEntityBoundingBox().minY - 1);
+                int posZ = MathHelper.floor(entity.posZ);
+                BlockPos posEntity = new BlockPos(posX, posY, posZ);
+                World world = entity.getEntityWorld();
+                IBlockState state = world.getBlockState(posEntity);
+
+                double random = Math.random() * 1000.0D + 1.0D;
+                double speed = Math.sqrt(entity.motionX * entity.motionX + entity.motionY * entity.motionY + entity.motionZ * entity.motionZ);
+
+                if (PathConfig.GeneralSettings.DEBUG)
+                {
+                    System.out.println("Random: " + random);
+                    System.out.println("Speed: " + speed);
+                }
+
+                if (!world.isRemote && state.getBlock() instanceof BlockRockVariant && speed > 0.08)
+                {
+                    BlockRockVariant blockRock = (BlockRockVariant) state.getBlock();
+
+                    if (blockRock.getType() == Rock.Type.GRASS || blockRock.getType() == Rock.Type.DRY_GRASS)
+                    {
+                        if (random < PathConfig.MobSettings.GRASS_TO_DIRT)
+                        {
+                            world.setBlockState(posEntity, BlockRockVariant.get(blockRock.getRock(), Rock.Type.DIRT).getDefaultState());
+                            BlockPos upPos = posEntity.up();
+                            Material upMaterial = world.getBlockState(upPos).getMaterial();
+                            if (upMaterial == Material.PLANTS || upMaterial == Material.VINE || world.getBlockState(upPos).getBlock() instanceof BlockPlacedItemFlat)
+                            {
+                                world.destroyBlock(upPos, false);
+                            }
+                            return;
+                        }
+                    }
+
+                    if (blockRock.getType() == Rock.Type.DIRT)
+                    {
+                        if (random < PathConfig.MobSettings.DIRT_TO_PATH)
+                        {
+                            world.setBlockState(posEntity, BlockRockVariant.get(blockRock.getRock(), Rock.Type.PATH).getDefaultState());
+                            return;
+                        }
+                    }
+
+                    if (blockRock.getType() == Rock.Type.PATH)
+                    {
+                        if (random < PathConfig.MobSettings.PATH_TO_GRAVEL)
+                        {
+                            world.setBlockState(posEntity, BlockRockVariant.get(blockRock.getRock(), Rock.Type.GRAVEL).getDefaultState());
+                            return;
+                        }
                     }
                 }
             }
