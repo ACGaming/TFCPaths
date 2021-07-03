78a40c120f14a5a6ae72fd525344cc057e6df143
ACGaming <4818419+ACGaming@users.noreply.github.com>
2021-07-03 11:39:35 +0200

    Proper bounding box and speed checks

--- src/main/java/mod/acgaming/tfcpaths/PathEvent.java
+++ src/main/java/mod/acgaming/tfcpaths/PathEvent.java
@@ -2,8 +2,9 @@ package mod.acgaming.tfcpaths;
 
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
@@ -17,24 +18,26 @@ import net.dries007.tfc.objects.blocks.stone.BlockRockVariant;
 @EventBusSubscriber
 public class PathEvent
 {
-    private static BlockPos posEntityOld = new BlockPos(0, 0, 0);
-
     @SubscribeEvent(priority = EventPriority.HIGHEST)
     public static void createPath(LivingEvent.LivingUpdateEvent event)
     {
         if (event.getEntity() != null)
         {
-            Entity entity = event.getEntity();
-            BlockPos posEntity = new BlockPos(entity.posX, entity.posY + entity.getYOffset(), entity.posZ);
+            EntityLivingBase entity = event.getEntityLiving();
+            int posX = MathHelper.floor(entity.posX);
+            int posY = MathHelper.floor(entity.getEntityBoundingBox().minY - 1);
+            int posZ = MathHelper.floor(entity.posZ);
+            BlockPos posEntity = new BlockPos(posX, posY, posZ);
             World world = entity.getEntityWorld();
             IBlockState state = world.getBlockState(posEntity);
 
             double random = Math.random() * 1000.0D + 1.0D;
+            double speed = Math.sqrt(entity.motionX * entity.motionX + entity.motionY * entity.motionY + entity.motionZ * entity.motionZ);
 
             if (PathConfig.DEBUG)
                 System.out.println(random);
 
-            if (!world.isRemote && !posEntityOld.equals(posEntity) && state.getBlock() instanceof BlockRockVariant && (Math.abs(entity.motionX) > 0.0D || Math.abs(entity.motionY) > 0.0D || Math.abs(entity.motionZ) > 0.0D))
+            if (!world.isRemote && state.getBlock() instanceof BlockRockVariant && speed > 0.08)
             {
                 BlockRockVariant blockRock = (BlockRockVariant) state.getBlock();
 
@@ -49,7 +52,6 @@ public class PathEvent
                         {
                             world.destroyBlock(upPos, false);
                         }
-                        posEntityOld = posEntity;
                         return;
                     }
                 }
@@ -59,7 +61,6 @@ public class PathEvent
                     if (random < PathConfig.DIRT_TO_PATH)
                     {
                         world.setBlockState(posEntity, BlockRockVariant.get(blockRock.getRock(), Rock.Type.PATH).getDefaultState());
-                        posEntityOld = posEntity;
                         return;
                     }
                 }
@@ -69,7 +70,6 @@ public class PathEvent
                     if (random < PathConfig.PATH_TO_GRAVEL)
                     {
                         world.setBlockState(posEntity, BlockRockVariant.get(blockRock.getRock(), Rock.Type.GRAVEL).getDefaultState());
-                        posEntityOld = posEntity;
                         return;
                     }
                 }
